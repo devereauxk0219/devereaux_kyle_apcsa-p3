e8ca5e350393225573c7e1a561215408e8f84c1e
devereauxk0219 <kyledevereaux@gmail.com>
2019-03-29 13:10:47 -0700

    Unit15Add

---
+++ Unit15/src/Ball.java
@@ -0,0 +1,81 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Kyle Devereaux
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ball extends Block
+{
+	private int xSpeed;
+	private int ySpeed;
+
+	public Ball()
+	{
+		super(200,200);
+		xSpeed = 3;
+		ySpeed = 1;
+	}
+
+	//add the other Ball constructors
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	   
+   //add the set methods
+	public void setXSpeed(int x)
+	{
+		xSpeed = x;
+	}
+	
+	public void setYSpeed(int y)
+	{
+		ySpeed = y;
+	}
+   
+   public void moveAndDraw(Graphics window)
+   {
+   	//draw a white ball at old ball location
+
+
+      setX(getX()+xSpeed);
+		//setY
+
+		//draw the ball at its new location
+   }
+   
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof Ball
+				&& ((Ball) obj).xSpeed == xSpeed
+				&& ((Ball) obj).ySpeed == ySpeed)
+		{
+			return true;
+		}
+		return false;
+	}   
+
+   //add the get methods
+	public int getXSpeed() {
+		return xSpeed;
+	}
+	
+	public int getYSpeed() {
+		return ySpeed;
+	}
+
+   //add a toString() method
+	public String toString()
+	{
+		return "x speed: " + xSpeed + "y speed: " + ySpeed;
+	}
+}
